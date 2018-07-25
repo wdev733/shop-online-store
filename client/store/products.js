ae2489a433a719d4c4cb8c6cc720921e54aff164
wdev733 <wdev733@gmail.com>
2018-07-25 17:12:23 -0400

    -thunk creator and actions created successfully for single product

--- client/store/products.js
+++ client/store/products.js
@@ -1,27 +1,62 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const allProducts = [];
+const initState = {
+  allProducts: [],
+  selectedProduct: {}
+}
+
+// ACTION TYPES
 const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS'
-function actionCreatorForProducts(content){
-    return{
-        type: RECEIVE_PRODUCTS,
-        payload: content
-    }
+const RECEIVE_SINGLE_PRODUCT = 'RECEIVE_SINGLE_PRODUCT'
+
+// ACTION CREATORS
+function actionCreatorForProducts(content) {
+  return {
+    type: RECEIVE_PRODUCTS,
+    payload: content
+  }
 }
 
-export const getAllProducts = ()=>{
-    return async (dispatch)=>{
-        const {data} = await axios.get('/api/products');
-        const action = actionCreatorForProducts(data);
-        dispatch(action);
-    }
+function actionCreatorForSingleProduct(content) {
+  return {
+    type: RECEIVE_SINGLE_PRODUCT,
+    payload: content
+  }
 }
 
-const reducer = function (state = allProducts, action){
-    switch (action.type){
-        case RECEIVE_PRODUCTS: return action.payload;
-        default: return state;
+export const getAllProducts = () => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('/api/products')
+      const action = actionCreatorForProducts(data)
+      dispatch(action)
+    } catch (error) {
+      console.log('Could not fetch all products', error)
     }
+  }
+}
+
+export const getSingleProduct = id => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get(`/api/products/${id}`)
+      const action = actionCreatorForSingleProduct(data)
+      dispatch(action)
+    } catch (error) {
+      console.log('Could not fetch single product', error)
+    }
+  }
+}
+
+const reducer = function(state = initState, action) {
+  switch (action.type) {
+    case RECEIVE_PRODUCTS:
+      return {...state, allProducts: action.payload}
+    case RECEIVE_SINGLE_PRODUCT:
+      return {...state, selectedProduct: action.payload}
+    default:
+      return state
+  }
 }
 
 export default reducer
--- client/store/products.spec.js
+++ client/store/products.spec.js
@@ -1,6 +1,6 @@
 import {expect} from 'chai'
 import {me, logout} from './user'
-import {getAllProducts} from './products'
+import {getAllProducts, getSingleProduct} from './products'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
@@ -10,29 +10,43 @@ import history from '../history'
 const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
 
-describe('thunk creators', ()=>{
-    let store
-    let mockAxios
+describe('thunk creators', () => {
+  let store
+  let mockAxios
 
-    const initialState = {products: []}
+  const initialState = {
+    products: [],
+    selectedProduct: {}
+  }
 
-    beforeEach(() => {
+  beforeEach(() => {
     mockAxios = new MockAdapter(axios)
     store = mockStore(initialState)
-    })
+  })
 
-    afterEach(() => {
+  afterEach(() => {
     mockAxios.restore()
     store.clearActions()
+  })
+  describe('get all products state', () => {
+    it('eventually dispatched the RECEIVE ALL PRODUCTS action', async () => {
+      const fakeProduct = require('../../testData/products')
+      mockAxios.onGet('/api/products').replyOnce(200, fakeProduct)
+      await store.dispatch(getAllProducts())
+      const actions = store.getActions()
+      expect(actions[0].type).to.be.equal('RECEIVE_PRODUCTS')
+      expect(actions[0].payload).to.be.deep.equal(fakeProduct)
     })
-    describe('get all products state', ()=>{
-        it('eventually dispatched the RECEIVE ALL PRODUCTS action', async ()=>{
-            const fakeProduct = require('../../testData/products');
-            mockAxios.onGet('/api/products').replyOnce(200, fakeProduct);
-            await store.dispatch(getAllProducts());
-            const actions = store.getActions();
-            expect(actions[0].type).to.be.equal('RECEIVE_PRODUCTS');
-            expect(actions[0].payload).to.be.deep.equal(fakeProduct);
-        })
+  })
+  describe('get selected product state', () => {
+    it('eventually dispatched the RECEIVED SINGLE PRODUCT action', async () => {
+      const testProduct = require('../../testData/products').products[0]
+      console.log('testProduct', testProduct)
+      mockAxios.onGet('/api/products/1').replyOnce(200, testProduct)
+      await store.dispatch(getSingleProduct(1))
+      const actions = store.getActions()
+      expect(actions[0].type).to.be.equal('RECEIVE_SINGLE_PRODUCT')
+      expect(actions[0].payload).to.be.deep.equal(testProduct)
     })
+  })
 })

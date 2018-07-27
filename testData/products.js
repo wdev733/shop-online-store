305a8bda7ae48320331f4c1158b38e571983b15b
wdev733 <wdev733@gmail.com>
2018-07-26 22:30:19 -0400

    tidied up cart route, wrote spec for it, tested and wrote cart reducer, cleaned up minor bugs

--- testData/products.js
+++ testData/products.js
@@ -1,9 +1,24 @@
 const products = [
-    {name: 'air ones', price: 100, picture: './pictures/airJordan.jpg'},
-    {name: 'Generic Shoes', price: 300, picture: './pictures/airJordan.jpg'},
-    {name: 'Nikes', price: 200, picture: './pictures/airJordan.jpg'},
+  {name: 'air ones', price: 100, picture: './pictures/airJordan.jpg'},
+  {name: 'Generic Shoes', price: 300, picture: './pictures/airJordan.jpg'},
+  {name: 'Nikes', price: 200, picture: './pictures/airJordan.jpg'}
+]
+
+const product = {
+  name: 'Clown Shoes',
+  price: 150,
+  picture: './pictures/airJordan.jpg'
+}
+
+const addedProducts = [
+  {name: 'air ones', price: 100, picture: './pictures/airJordan.jpg'},
+  {name: 'Generic Shoes', price: 300, picture: './pictures/airJordan.jpg'},
+  {name: 'Nikes', price: 200, picture: './pictures/airJordan.jpg'},
+  {name: 'Clown Shoes', price: 150, picture: './pictures/airJordan.jpg'}
 ]
 
 module.exports = {
-    products
+  products,
+  product,
+  addedProducts
 }

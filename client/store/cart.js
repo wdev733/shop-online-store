2e394007e26fe81bd9b0047056ed5ebc28eba6d3
wdev733 <wdev733@gmail.com>
2018-07-27 13:45:06 -0400

    moved to front end

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,8 +1,49 @@
 import axios from 'axios'
 
+// CART FRAMEWORK
+
+class Cart {
+  constructor(inProductsWithSettings = []) {
+    this.products = inProductsWithSettings
+  }
+  get total() {
+    return this.products.reduce((accum, product) => {
+      return accum + product.price
+    }, 0)
+  }
+  addProduct(product, quantity = 1) {
+    const alreadyExists = this.getProduct(product.id)
+    if (!alreadyExists) {
+      const cloneOfProduct = Object.assign({}, ...product, quantity)
+      this.products.push(cloneOfProduct)
+    }
+  }
+  getProduct(id) {
+    return this.products.find(product => {
+      return product.id === id
+    })
+  }
+  incrementQuantity(id) {
+    const product = this.getProduct(id)
+    if (product) {
+      product.quantity += 1
+    }
+  }
+  setQuantity(id, quantity) {
+    const product = this.getProduct(id)
+    if (product) {
+      product.quantity = quantity
+    }
+  }
+  clearCart() {
+    this.products = []
+  }
+}
+
 // ACTION TYPES //
 const GET_CART = 'GET_CART'
 const CLEAR_CART = 'CLEAR_CART'
+const ADD_CART = 'ADD_CART'
 
 // ACTION CREATORS //
 const getCart = cart => ({
@@ -18,7 +59,7 @@ const clearCart = () => ({
 export const fetchCart = () => async dispatch => {
   try {
     const res = await axios.get(`/api/carts`)
-    const cart = res.data
+    const cart = new Cart(res.data)
     dispatch(getCart(cart))
   } catch (error) {
     console.error(error)
@@ -47,5 +88,12 @@ const cartReducer = (state = cart, action) => {
       return state
   }
 }
+
+const updateCart = async () => {
+  const {cart} = store.getState()
+  await axios.post('/api/carts', cart.products)
+}
+
+store.subscribe(updateCart)
 
 export default cartReducer
--- server/api/cart.js
+++ server/api/cart.js
@@ -1,44 +1,9 @@
 const router = require('express').Router()
 
-class Cart {
-  constructor() {
-    this.products = []
-  }
-  get total() {
-    return this.products.reduce((accum, product) => {
-      return accum + product.price
-    }, 0)
-  }
-  addProduct(product, quantity = 0) {
-    const alreadyExists = this.getProduct(product.id)
-    if (!alreadyExists) {
-      const cloneOfProduct = Object.assign({}, ...product, quantity)
-      this.products.push(cloneOfProduct)
-    }
-  }
-  getProduct(id) {
-    return this.products.find(product => {
-      return product.id === id
-    })
-  }
-  incrementQuantity(id) {
-    const product = this.getProduct(id)
-    if (product) {
-      product.quantity += 1
-    }
-  }
-  setQuantity(id, quantity) {
-    const product = this.getProduct(id)
-    if (product) {
-      product.quantity = quantity
-    }
-  }
-}
-
 router.get('/', (req, res, next) => {
   try {
     if (!req.session.cart) {
-      req.session.cart = []
+      req.session.cart = {products: []}
     }
     res.json(req.session.cart)
   } catch (err) {
@@ -49,7 +14,7 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   try {
     if (!req.session.cart) {
-      req.session.cart = []
+      req.session.cart = {products: []}
     }
     req.session.cart.push(req.body)
     res.status(201).json(req.session.cart)

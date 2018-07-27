4271a7402897cc7927ff2ae0b084fc4a1bb69d47
wdev733 <wdev733@gmail.com>
2018-07-27 15:51:40 -0400

    Update cart.js
    removed comments

--- client/store/cart.js
+++ client/store/cart.js
@@ -2,53 +2,6 @@ import axios from 'axios'
 
 // CART FRAMEWORK
 
-// cart = new Cart()
-// cart.products
-// cart.total
-
-// class Cart {
-//   constructor(inProducts = []) {
-//     this.products = inProducts
-//     []
-//   }
-//   get total() {
-//     return this.products.reduce((accum, product) => {
-//       return accum + product.price
-//     }, 0)
-//   }
-//   addProduct(product, quantity = 1) {
-//     const alreadyExists = this.getProduct(product.id)
-//     if (!alreadyExists) {
-//       const cloneOfProduct = Object.assign({}, ...product, quantity)
-//       this.products.push(cloneOfProduct)
-//     }
-//     return this.products
-//   }
-//   getProduct(id) {
-//     return this.products.find(product => {
-//       return product.id === id
-//     })
-//   }
-//   incrementQuantity(id) {
-//     const product = this.getProduct(id)
-//     if (product) {
-//       product.quantity += 1
-//     }
-//   }
-//   setQuantity(id, quantity) {
-//     const product = this.getProduct(id)
-//     if (!product){
-
-//     }
-//     if (product) {
-//       product.quantity = quantity
-//     }
-//   }
-//   clearCart() {
-//     this.products = []
-//   }
-// }
-
 // ACTION TYPES //
 const GET_CART = 'GET_CART'
 const CLEAR_CART = 'CLEAR_CART'

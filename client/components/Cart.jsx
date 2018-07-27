bf11a93188b8be82c52442db30d5666a25cff826
wdev733 <wdev733@gmail.com>
2018-07-27 15:03:06 -0400

    cart view displaying

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -1,13 +1,24 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import fetchCart from '../store/cart'
+import {Table} from 'react-bootstrap'
 
 const dummyCart = [
   {
+    id: 3,
     name: 'Clown Shoes',
     price: 150,
-    picture: './pictures/airJordan.jpg',
+    picture:
+      'https://specials-images.forbesimg.com/imageserve/56ce157fe4b062f6b59a7bf7/416x416.jpg?background=000000&cropX1=0&cropX2=744&cropY1=95&cropY2=839',
     inventory: 3
+  },
+  {
+    id: 3,
+    name: 'Converse',
+    price: 99,
+    picture:
+      'https://static.highsnobiety.com/wp-content/uploads/2017/06/05200653/converse-one-piece-chucks-01-480x320.jpg',
+    inventory: 2
   }
 ]
 
@@ -17,7 +28,34 @@ class Cart extends Component {
   }
 
   render() {
-    return <h1>made it to cart</h1>
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dummyCart.map(product => (
+            <tr key={product.id}>
+              <td>
+                <span>
+                  <img
+                    src={product.picture}
+                    style={{height: 'auto', width: '75px', marginRight: '15px'}}
+                  />
+                </span>
+                {product.name}
+              </td>
+              <td>${product.price}</td>
+              <td>1</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    )
   }
 }
 

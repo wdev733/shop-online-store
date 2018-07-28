20174d2f97bf6a4c5b4e298091e1805caf41ea6d
wdev733 <wdev733@gmail.com>
2018-07-28 19:51:11 -0400

    Update index.js
    removed comment and unnecesary import

--- client/store/index.js
+++ client/store/index.js
@@ -8,12 +8,10 @@ import cart from './cart'
 import axios from 'axios'
 import {persistStore, persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2'
 
 const persistConfig = {
   key: 'root',
   storage: storage
-  // stateReconciler: autoMergeLevel2
 }
 
 const reducer = combineReducers({user, products, cart})

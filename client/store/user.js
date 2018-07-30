935b2ef556484cd8faaadfbb9e91c2cbce57735e
wdev733 <wdev733@gmail.com>
2018-07-30 18:03:29 -0400

    Update user.js

--- client/store/user.js
+++ client/store/user.js
@@ -8,6 +8,11 @@ const GET_USER = 'GET_USER'
 const REMOVE_USER = 'REMOVE_USER'
 
 /**
+ * INITIAL STATE
+ */
+const defaultUser = {}
+
+/**
  * ACTION CREATORS
  */
 const getUser = user => ({type: GET_USER, user})
@@ -19,7 +24,7 @@ const removeUser = () => ({type: REMOVE_USER})
 export const me = () => async dispatch => {
   try {
     const res = await axios.get('/auth/me')
-    dispatch(getUser(res.data || {}))
+    dispatch(getUser(res.data || defaultUser))
   } catch (err) {
     console.error(err)
   }
@@ -54,12 +59,12 @@ export const logout = () => async dispatch => {
 /**
  * REDUCER
  */
-export default function(state = {}, action) {
+export default function(state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
       return action.user
     case REMOVE_USER:
-      return {}
+      return defaultUser
     default:
       return state
   }

d9afee8b4f3d56ab084c1ab50ad00b1e797bba9d
wdev733 <wdev733@gmail.com>
2018-07-31 18:05:09 -0400

    wrote spec for order route

--- server/api/order.spec.js
+++ server/api/order.spec.js
@@ -1,17 +1,50 @@
 const {expect} = require('chai')
 const request = require('supertest')
 const app = require('../')
-const {order} = require('../../testData/order')
+const {productSize} = require('../../testData/order')
 const db = require('../db')
+const {Order, ProductSize, Product, Size} = require('../db/models')
 
 describe('/api/order', () => {
   beforeEach(async () => {
     await db.sync({force: true})
+    await Promise.all([
+      Product.create({
+        id: 5,
+        name: 'air ones',
+        price: 100,
+        picture: '/pictures/airJordan.jpg'
+      }),
+      Size.create({
+        size: 7
+      })
+    ])
+    await ProductSize.create(productSize)
   })
-  xit('', async () => {
+  it('places an object in cart into the order properly', async () => {
+    const body = {
+      password: process.env.ORDER_SECRET,
+      name: 'John Smith',
+      cart: [
+        {
+          id: 5,
+          name: 'air ones',
+          price: 100,
+          quantity: 2,
+          size: 7,
+          picture: './pictures/airJordan.jpg'
+        }
+      ]
+    }
     await request(app)
       .post('/api/order')
-      .send(order)
+      .send(body)
       .expect(201)
+
+    const orderDb = await Order.findById(1)
+    // expect(orderDb.customer).to.equal(body.name)
+    // console.log(`testing:`, orderDb.quanitty, body.quantity)
+    expect(orderDb.quantity).to.equal(body.cart[0].quantity)
+    expect(orderDb.productSizeId).to.equal(productSize.id)
   })
 })

e11c780b5bf4c8d8bff06ddc3f81133185059d09
wdev733 <wdev733@gmail.com>
2018-08-01 09:14:33 -0400

    Better message on checkout

--- client/components/SuccessfulPurchase.js
+++ client/components/SuccessfulPurchase.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const SuccessfulPurchase = () => {
-  return <div>Happy!</div>
+  return (
+    <div className="center">
+      Your Order has been confirmed. Thank you for your purchase!
+    </div>
+  )
 }
 
 export default SuccessfulPurchase
